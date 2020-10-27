72287c708167752498268c5dc021a30ab0ea48a4
Fynn <fynnhunt@hotmail.co.uk>
2020-10-27 01:14:12 +0000

    Turned off compression in build

--- Build/DeathrunWebGL.asm.code.js
+++ Build/DeathrunWebGL.asm.code.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98bdcf9bf9628c7d351e968c6d26664c850c6ba8a78835f4eebe51c63aca369b
-size 154561341
+oid sha256:baf941043d5e23b6aaa1812db32b0c0ddd555b66da179e58708e5a9b625a0409
+size 154558203
